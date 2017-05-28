7fbf1deb07879c48555456f8b1057c41a30d589c
sgomezp <sgomezp@gmail.com>
2017-05-28 18:36:05 +0200

    Finish Project

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -13,16 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
-import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,72 +31,18 @@ public class MainActivity extends AppCompatActivity  {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        // Find the View that shows the numbers category
-        TextView numbers = (TextView) findViewById(R.id.numbers);
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        // Set  a click listener on that View
-        numbers.setOnClickListener(new OnClickListener() {
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
 
-            // The code in this method will be execute when the numbers category is clicked on.
-            @Override
-            public void onClick(View view) {
-                // Create a new intent to open the {@link NumbersActivity}
-                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
 
-                // Start the new activity
-                startActivity(numbersIntent);
-            }
-        });
-
-        // Find the View that shows the family category
-        TextView family = (TextView) findViewById(R.id.family);
-
-        // Set a click listener to that View
-        family.setOnClickListener(new OnClickListener() {
-
-            // The code in this method will be execute when the family category is clicked on.
-
-            @Override
-            public void onClick(View v) {
-                // Create a new Intent to open the {@link FamilyActivities}
-                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
-
-                //Start the new activity
-                startActivity(familyIntent);
-            }
-        });
-
-        // Find the View thet shows colors category
-        TextView colors = (TextView) findViewById(R.id.colors);
-
-        // Set a click listener to that View
-        colors.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Create a new Intent to open the {@link ColorsActivity}
-                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
-
-                // Start the activity
-                startActivity(colorsIntent);
-            }
-        });
-
-        // Find the View that shoes Phrases category
-        TextView phrases = (TextView) findViewById(R.id.phrases);
-
-        // Set a click listener to that View
-        phrases.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Create a new Intent to open the {@link PhrasesActivity}
-                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-
-                // Start the activity
-                startActivity(phrasesIntent);
-            }
-        });
-
-
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
 
